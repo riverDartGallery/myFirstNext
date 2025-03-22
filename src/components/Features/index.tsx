e0258fb02c8e68ca4a73fb63b88891d2fa4b1d42
Jason Browning <39011117+riverDartGallery@users.noreply.github.com>
2025-03-22 12:16:30 +0000

    Update Features section title to clarify branch name usage

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,7 +8,7 @@ const Features = () => {
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features!!!! Using branch name test-4"
+            title="Main Features!!!! Using branch name test-4 for testing"
             paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
             center
           />
