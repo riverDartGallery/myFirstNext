3759417799f1213338ad76ab7c6001a353478709
Jason Browning <39011117+riverDartGallery@users.noreply.github.com>
2025-03-22 11:26:52 +0000

    first commit

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  Free!!! and Open-Source Next.js Template for Startup & SaaS
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   Startup is free Next.js template for startups and SaaS
@@ -23,7 +23,7 @@ const Hero = () => {
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
-                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    className="rounded-full bg-orange-500 px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-orange-600"
                   >
                     🔥 Get Pro
                   </Link>
